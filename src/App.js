3ef967a656177380a247d3da5cf9709a87a7a684
Indra Saepudin <indrasaepudin212@gmail.com>
2025-03-24 17:05:07 +0700

    Doneee

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Services from "./pages/services";
+import Services from "./pages/Services";
 import ProductDetail from "./pages/ProductDetail";
 
 function App() {
